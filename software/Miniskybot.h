4d5192cc542e7d45e8bf3cbfb2dcd1f23bce369e
David Estevez Fernandez <david.estevez.fdez@gmail.com>
2012-06-08 12:37:59 +0200

    Added Miniskybot.h

---
+++ software/Miniskybot.h
@@ -0,0 +1,20 @@
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//-- Miniskybot library
+//------------------------------------------------------------------------------
+//-- Simplify working and programming a Miniskybot (or similar) robot by using
+//-- this library.
+//--
+//-- It contains functions to control a pair of motors and some sensors. 
+//--
+//-- For more info, read README
+//------------------------------------------------------------------------------
+//-- Author:
+//-- David Estévez-Fernández, Jun 2012
+//-- GPL license
+//------------------------------------------------------------------------------
+//-- Requires a Miniskybot  robot, design by Juan González-Gómez (Obijuan):
+//-- https://github.com/Obijuan/Miniskybot
+//------------------------------------------------------------------------------
+
+
