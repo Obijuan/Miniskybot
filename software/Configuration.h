d0024885c767e00e50e9cbe25af8ae71cb9bf20b
David Estevez Fernandez <david.estevez.fdez@gmail.com>
2012-06-12 16:42:01 +0200

    First example added

--- software/Configuration.h
+++ software/Configuration.h
@@ -16,3 +16,20 @@ const float CALIBRATION_PARAM[MAX_SENSORS_IR][2] = {
 #define US_3PIN 1 	//-- Ultrasound sensor, 3 pin
 #define US_4PIN 2 	//-- Ultrasound sensor, 4 pin
 
+
+//-- Table containing all the pairs (value/velocitý)
+#define NUM_VALUES 12
+const float VELOCITY_TABLE[NUM_VALUES][2] = {
+	{ 14.5/2, 255 },
+	{ 12.5/2, 235},
+	{ 12.0/2, 215},
+	{ 12.0/2, 195},
+	{ 11.0/2, 175},
+	{ 10.0/2, 155},
+	{  9.0/2, 135},
+	{  7.0/2, 115},
+	{  6.0/2,  95},
+	{  4.0/2,  75},
+	{  3.0/2,  65},
+	{  2.0/2,  55}
+};
